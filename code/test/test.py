441a0841875bb86f3c859ebc7b85a4413fcfe320
yoozze <ezzooy@gmail.com>
2019-11-20 21:10:22 +0100

    Updated python test

--- code/test/test.py
+++ code/test/test.py
@@ -1,41 +1,50 @@
+from streamdm import HoeffdingTree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.metrics import classification_report
+from sklearn.datasets import load_iris
 import numpy as np
-import pandas as pd
-import streamdm as sdm
-from scipy.io import arff
-from sklearn.model_selection import train_test_split
+import time
 
+methods = [
+    {
+        'name': 'Decision Tree (scikit-learn)',
+        'ctor': DecisionTreeClassifier
+    },
+    {
+        'name': 'Hoeffding Tree (streamDM)',
+        'ctor': HoeffdingTree
+    }
+]
 
-with open("data.arff") as f:
-    learner = sdm.HoeffdingTree()
-    print(f'Method: {learner}')
+# Prepare data
+iris = load_iris()
+X = iris.data
+y = iris.target
+print(iris.DESCR)
 
-    data, meta = arff.loadarff(f)
-    print(meta)
-    print(f'\nDataset {data.shape}: \n{data}')
+for method in methods:
+    print('\n' + '=' * 53)
+    print(method['name'])
+    print('=' * 53 + '\n')
 
-    set_size = 1000
-    df = pd.DataFrame(data[0:set_size])
-    X = df.iloc[:, 0:13].to_numpy()
-    y = df.iloc[:, 13].to_numpy().astype(int)
-    print(f'\nX {X.shape}: \n{X}')
-    print(f'\ny {y.shape}: \n{y}')
+    kf = KFold(n_splits=10)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False)
-    print(f'\nX_train {X_train.shape}: \n{X_train}')
-    print(f'\ny_train {y_train.shape}: \n{y_train}')
-    print(f'\nX_test {X_test.shape}: \n{X_test}')
-    print(f'\ny_test {y_test.shape}: \n{y_test}')
+    t = time.time()
+    y_pred = []
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-    learner.fit(X_train, y_train)
-    predictions = learner.predict(X_test)
-    print(f'\npredictions {predictions.shape}: \n{predictions}')
-    print('Finished!')
+        # Build model
+        learner = method['ctor']()
+        learner.fit(X_train, y_train)
 
-    # learner.export_json("HoeffdingTree.json")
-    # print('Model saved!')
-    # 
-    # learner1 = sdm.HoeffdingTree()
-    # learner1.import_json("HoeffdingTree.json")
-    # print('Model loaded!')
-    # predictions1 = learner1.predict(X_test)
-    # print(f'\npredictions {predictions.shape}: \n{predictions}')
+        # Predict
+        pred = learner.predict(X_test)
+        y_pred += pred.tolist()
+
+    print(f'Time: {time.time() - t}\n')
+
+    # Report
+    print(classification_report(y, y_pred, target_names=iris.target_names))
